bdf83f8d55fcc1e00b6092c894627d7e448f8178
kelsonthony <k.thony@gmail.com>
2023-09-10 22:58:03 -0300

    Adicionado application properties

--- src/main/java/com/kelsonthony/lojavirtual/LojaVirtualApplication.java
+++ src/main/java/com/kelsonthony/lojavirtual/LojaVirtualApplication.java
@@ -2,8 +2,16 @@ package com.kelsonthony.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.kelsonthony.lojavirtual.domain.model")
+@ComponentScan(basePackages = {"com.*"})
+@EnableJpaRepositories(basePackages = "com.kelsonthony.lojavirtual.domain.repository")
+@EnableTransactionManagement
 public class LojaVirtualApplication {
 
 	public static void main(String[] args) {
